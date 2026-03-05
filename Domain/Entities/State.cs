686bd3e28acf7678b762e46be266e3475b9d2950
heredikshit-ux <jaanilyrical@gmail.com>
2026-03-05 10:12:38 +0530

    Domain - Entities(Country, State, City)

---
+++ Domain/Entities/State.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class State
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int CountryId { get; set; }
+    public Country? Country { get; set; }
+}
